6006487612edf5901e97e9d42d022497f32b7262
pridhars <141481194+pridhars@users.noreply.github.com>
2023-08-07 11:28:09 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,58 @@
+#double quotes string
+a="priyadharshini"
+print(a)
+print(type(a))
+#single quotes string
+a='priyadharshini'
+print(a)
+#string type variables
+name='priyadharshini'
+print(name)
+#access string charater
+name='priyadharshini'
+print(name[5])
+print(name[-7])
+print(name[4:-8])
+print(name[1:])
+#name='priyadharshini'
+#name[3]='u'
+#print(name)
+myname='priya'
+myname='dharshini'
+print(myname)
+#multiline string
+message="""be positve
+be yourself
+be bold"""
+print(message)
+#compare two strings
+str1='priyadharshini'
+str2='dharshini'
+str3='priyadharshini'
+print(str1==str2)
+print(str3==str1)
+#join two string
+name='priya'
+myname='dharshini'
+result=name+myname
+print(result)
+name='priyadharshini'
+print(len(name))
+name='priyadharshini'
+print('e' in name )
+print('a' in name)
+#iteration
+name='priyadharshini'
+for letter in name:
+   print(letter)
+#uppercase
+name='priyadharshini'
+print(name.upper())
+#lowercase
+NAME="PRIYADHARSHINI"
+print(name.lower())
+#partition
+name='priyadharshini'
+print(name.partition('dharshini'))
+
+
