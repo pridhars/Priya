9deb25f235344d181615bec2346f34d7b84484a5
pridhars <141481194+pridhars@users.noreply.github.com>
2023-08-07 11:41:06 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,4 @@
+values=int(input("enter the number"))
+for i in values:
+    print(i)
+
---
+++ while.py
@@ -0,0 +1,7 @@
+#reverse the number using while loop
+n=int(input("Enter the number:"))
+while n>=1:
+    print(n,end=" ")
+    n=n-1
+
+
