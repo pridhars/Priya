bfb8635ecadc1528c41e230c35594081e7a7c416
pridhars <141481194+pridhars@users.noreply.github.com>
2023-08-09 13:32:11 +0530

    Delete if and else.py

--- if and else.py
+++
@@ -1,9 +0,0 @@
-#to check the health conditions
-temperature=int (input("Enter the temperature:"))
-if temperature>92:
-    print("Eye condition is dry ")
-    print("Patient should consume lots of water")
-    print("Consult to the doctor")
-else:
-    print("Eye condition is normal")
-    print("The person is healthy")
